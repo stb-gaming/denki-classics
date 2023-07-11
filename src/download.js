2fb4e8b59a4ef69e9c578dfc06b8a42040ceece8
cobaltgit <cobaltmail4204@gmail.com>
2023-07-11 14:57:39 +0100

    Add functions for downloading files

---
+++ src/download.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const fs = require("fs/promises");
+
+const downloadFile = async (url, destPath) => {
+    const filename = url.split("/").pop();
+    const res = await fetch(url);
+    const buf = Buffer.from(await res.arrayBuffer());
+    const destination = path.join(destPath, filename)
+
+    try {
+        await fs.access(destination, fs.constants.R_OK)
+    } catch (err) {
+        fs.writeFile(destination, Buffer.from(buf), err => {
+            if (err) return console.error(err);
+        })
+    }
+}
+
+const downloadGame = async (id, gamesPath) => {
+    const files = ["app.html", "app.js", "app.wasm", "app.data"];
+    const gameUrl = `https://denki.co.uk/sky/${id}/`;
+    const destPath = path.join(gamesPath, id);
+
+    try {
+        await fs.access(destPath, fs.constants.R_OK)
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            await fs.mkdir(destPath)
+        }
+    }
+
+    await Promise.all(files.map(file => downloadFile(gameUrl + file, destPath)));
+}
+
+module.exports = {
+    downloadFile, downloadGame
+}
